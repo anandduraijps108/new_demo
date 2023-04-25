c8d5df659860e8ba348156a6c312eee0dfb6b9c0
anandduraijps108 <anandjps.108@gmail.com>
2023-04-25 14:49:14 +0530

    updates

--- src/Authendication/SignIn/index.test.js
+++ src/Authendication/SignIn/index.test.js
@@ -1,37 +1,49 @@
-    import React from 'react'
-    import LoginPage from "./index"
-    import { BrowserRouter } from 'react-router-dom';
-    import {render, screen, waitFor} from '@testing-library/react'
-    
-    beforeEach(()=>{
-        
-        render(<BrowserRouter>
-            <LoginPage/>
-          </BrowserRouter>);
-    });
+import { render, fireEvent, waitFor, screen } from "@testing-library/react";
+import SignIn from "./SignIn";
 
-    describe('Login test-01', () => {
-     it('signIn',()=>{
-        var signInText =  screen.getByTestId('signin-text');
-        expect(signInText).not.toBeUndefined();
-     })   
-   
-   });
+describe("SignIn component", () => {
+  test("Renders sign-in form with email and password inputs and submit button", () => {
+    render(<SignIn />);
+    expect(screen.getByText("Sign In")).toBeInTheDocument();
+    expect(screen.getByLabelText("Email:")).toBeInTheDocument();
+    expect(screen.getByLabelText("Password:")).toBeInTheDocument();
+    expect(screen.getByRole("button", { name: "Sign In" })).toBeInTheDocument();
+  });
 
-   
-   describe("Login test-02", () => {
-     test("Submit button click", async () => {
-       const onSubmit = jest.fn().mockResolvedValue({
-         email: "anand",
-         password: "123",
-       });
+  test("Shows error message when email or password is empty on form submit", async () => {
+    render(<SignIn />);
+    fireEvent.click(screen.getByRole("button", { name: "Sign In" }));
+    await waitFor(() =>
+      expect(screen.getByText("Please enter the email address.")).toBeInTheDocument()
+    );
+  });
 
-       const eMail = screen.getByPlaceholderText("Email Address*");
-       const password = screen.getByPlaceholderText("Password*");
+  test("Shows error message for invalid credentials", async () => {
+    const axios = require("axios");
+    jest.mock("axios");
+    axios.post.mockRejectedValue({ response: { data: { error: "invalid credentials" } } });
+    render(<SignIn />);
+    fireEvent.change(screen.getByLabelText("Email:"), { target: { value: "test@test.com" } });
+    fireEvent.change(screen.getByLabelText("Password:"), { target: { value: "test123" } });
+    fireEvent.click(screen.getByRole("button", { name: "Sign In" }));
+    await waitFor(() =>
+      expect(screen.getByText("Invalid credentials")).toBeInTheDocument()
+    );
+  });
 
-       expect(eMail).not.toBeUndefined();
-       expect(password).not.toBeUndefined();
-       await onSubmit();
-       expect(onSubmit).toBeCalledTimes(1);
-     });
-   });
+  test("Navigates to home page on successful login", async () => {
+    const axios = require("axios");
+    jest.mock("axios");
+    axios.post.mockResolvedValue({ data: { token: "testtoken" } });
+    const navigate = jest.fn();
+    jest.mock("react-router-dom", () => ({
+      ...jest.requireActual("react-router-dom"),
+      useNavigate: () => navigate,
+    }));
+    render(<SignIn />);
+    fireEvent.change(screen.getByLabelText("Email:"), { target: { value: "test@test.com" } });
+    fireEvent.change(screen.getByLabelText("Password:"), { target: { value: "test123" } });
+    fireEvent.click(screen.getByRole("button", { name: "Sign In" }));
+    await waitFor(() => expect(navigate).toHaveBeenCalledWith("/"));
+  });
+});
--- src/Dashboard/Home/index.test.js
+++ src/Dashboard/Home/index.test.js
@@ -1,24 +1,38 @@
-import React from 'react'
-import HomePage from "./index"
+import React from 'react';
+import HomePage from './index';
 import { BrowserRouter } from 'react-router-dom';
-import {render, screen, waitFor} from '@testing-library/react'
+import { render, screen, waitFor } from '@testing-library/react';
 
-  
+test('HomePage renders without errors', async () => {
+  const onSubmit = jest.fn().mockResolvedValue({
+    post: [
+      {
+        date: '2023-04-04',
+        description: 'Bying some carrots',
+        amount: '400',
+        id: 2,
+      },
+    ],
+  });
 
-test('App', async () => {
+  await onSubmit();
+  expect(onSubmit).toHaveBeenCalled();
 
-    const onSubmit = jest.fn().mockResolvedValue({
-       "post": [
-            {
-              "date": "2023-04-04",
-              "description": "Bying some carrots",
-              "amount": "400",
-              "id": 2
-            }
-          ]
-      });
-  
-   await onSubmit();
-   expect(onSubmit).toBeTruthy();
-  })
+  render(
+    <BrowserRouter>
+      <HomePage onSubmit={onSubmit} />
+    </BrowserRouter>
+  );
 
+  await waitFor(() => {
+    const dateInput = screen.getByLabelText('Date');
+    const descriptionInput = screen.getByLabelText('Description');
+    const amountInput = screen.getByLabelText('Amount');
+    const submitButton = screen.getByRole('button', { name: 'Submit' });
+
+    expect(dateInput).toBeInTheDocument();
+    expect(descriptionInput).toBeInTheDocument();
+    expect(amountInput).toBeInTheDocument();
+    expect(submitButton).toBeInTheDocument();
+  });
+});
